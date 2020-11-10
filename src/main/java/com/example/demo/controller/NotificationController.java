fdedd91c457c77be3e236998aeaa0b2ece065338
quocnna <63178225+quocnna@users.noreply.github.com>
2020-11-10 10:51:20 +0700

    updated

--- src/main/java/com/example/demo/controller/NotificationController.java
+++ src/main/java/com/example/demo/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Account;
 import com.example.demo.model.Account$;
 import com.example.demo.model.Cote;
+import com.example.demo.model.FeedType;
 import com.speedment.jpastreamer.application.JPAStreamer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
--- src/main/java/com/example/demo/service/BaseService.java
+++ src/main/java/com/example/demo/service/BaseService.java
@@ -1,6 +1,11 @@
 package com.example.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BaseService<T> {
+    List<T> getAll(int pageNum);
+    Optional<T> getById(int id);
     void save(T t);
     void delete(int[] ids);
 }
