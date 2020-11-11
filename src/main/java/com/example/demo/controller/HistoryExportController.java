34cf31612f060e64e72e8c3fb5558e4678eff434
tuongvule <tuongvule@gmail.com>
2020-11-11 15:48:12 +0700

    ff

--- src/main/java/com/example/demo/controller/HistoryExportController.java
+++ src/main/java/com/example/demo/controller/HistoryExportController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 import com.example.demo.model.HistoryExport;
+import com.example.demo.model.Stock;
 import com.example.demo.service.impl.HistoryExportServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +19,16 @@ public class HistoryExportController {
     @GetMapping("/export-management")
     public List<HistoryExport> getAll(){
         return this.historyExportService.getAll();
+    }
+
+    //Creator Tuong
+    // add new HistoryExport
+    @PostMapping("/export-management")
+    public void addExportManagement(@RequestBody HistoryExport historyExport){
+        try {
+            historyExportService.save(historyExport);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
