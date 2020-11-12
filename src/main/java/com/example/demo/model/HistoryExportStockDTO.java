e1f3d66b1c58f9b32a75988e84bad8539bf3a5c7
tuongvule <tuongvule@gmail.com>
2020-11-12 13:41:24 +0700

    z

--- src/main/java/com/example/demo/model/HistoryExportStockDTO.java
+++ src/main/java/com/example/demo/model/HistoryExportStockDTO.java
@@ -1,5 +1,23 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class HistoryExportStockDTO {
-    
+    private int id;
+    private String type;
+    private String shipmentCode;
+    private String feedTypeName;
+    private String vendorName;
+    private LocalDate exportDate;
+    private int quantityExport;
+    private String unit;
+    private String exportEmployeeName;
+    private String receiveEmployeeName;
 }
