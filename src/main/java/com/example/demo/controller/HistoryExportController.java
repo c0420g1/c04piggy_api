1376194981f1492aa2961b50d27bde0fbceed624
Dino Hiếu <hieufix1710@gmail.com>
2020-11-18 10:22:32 +0700

    hieu-FE

--- src/main/java/com/example/demo/controller/HistoryExportController.java
+++ src/main/java/com/example/demo/controller/HistoryExportController.java
@@ -2,20 +2,12 @@ package com.example.demo.controller;
 import com.example.demo.model.HistoryExport;
 import com.example.demo.model.HistoryExportDTO;
 import com.example.demo.model.HistoryExportStockDTO;
-import com.example.demo.model.StockDTO;
 import com.example.demo.service.impl.CoteServiceImpl;
 import com.example.demo.service.impl.HistoryExportServiceImpl;
 import com.example.demo.service.impl.StockServiceImpl;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-
+import com.speedment.common.json.Json;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -55,17 +47,20 @@ public class HistoryExportController {
         }
         return 0;
     }
-    @PutMapping("/exportCote")
-    public int exportCote(@RequestBody HistoryExport historyExport, @RequestParam int idCote){
-        return historyExportService.exportAllInCote(idCote,historyExport);
-    }
     @PutMapping("/exportPigs")
     public int exportPigs(@RequestBody HistoryExport historyExport, @RequestParam String ids){
+        System.out.println(ids);
         String[] idPigs = ids.split(",");
-        int[] idSold = new int[ids.length()];
-        for (int i = 0; i < idPigs.length; i++) {
-            idSold[i] = Integer.parseInt(idPigs[i]);
+        int[] idSold = new int[idPigs.length];
+        try {
+
+            for (int i = 0; i < idPigs.length; i++) {
+                idSold[i] = Integer.parseInt(idPigs[i]);
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
         }
+
       return   historyExportService.addPigExport(idSold, historyExport);
     }
 //
