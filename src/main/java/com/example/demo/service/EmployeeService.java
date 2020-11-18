49efb8b4f968e3316684b4ddbc6419e71b5038d2
viet <voducviet08b2@gmail.com>
2020-11-18 08:46:39 +0700

    abc

--- src/main/java/com/example/demo/service/EmployeeService.java
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -9,6 +9,5 @@ import java.util.Optional;
 
 public interface EmployeeService extends BaseService<Employee> {
     List<EmployeeDTO> search(int pageNumber, int pageSize, String search);
-//    List<EmployeeDTO> getAllEmployeeDTO(int pageNumber, String search);
     Optional<EmployeeDTO> getByIdDTO(int id);
 }
